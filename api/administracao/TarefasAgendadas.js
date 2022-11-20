54acd59e60a47477a85a87ed01bd5c9501cfb953
gutoconde <gutoconde@gmail.com>
2022-11-20 18:19:25 -0300

    Alterando cron expression

--- api/administracao/TarefasAgendadas.js
+++ api/administracao/TarefasAgendadas.js
@@ -3,7 +3,7 @@ const cron = require('node-cron');
 const TarefaImportacaoDados = require('../model/TarefaImportacaoDados');
 
 module.exports = (db) => {
-	cron.schedule('00 00 18 * * *', () => {
+	cron.schedule('00 0/10 * * * *', () => {
 		const task = new TarefaImportacaoDados();
 		task.executar(db);
 	});
--- api/model/TarefaImportacaoDados.js
+++ api/model/TarefaImportacaoDados.js
@@ -3,11 +3,13 @@ const serviceFactory = require('../model/ServiceFactory');
 class TarefaImportacaoDados {
 
     async executar(db) {
+        console.log('Iniciando importacao de dados');
         const servicoImportacao = serviceFactory.getServicoImportacaoGabinete(db);
         servicoImportacao.importarGabinetes();
 
         const servicoImportacaoServidor = serviceFactory.getServicoImportacaoServidor(db);
         servicoImportacaoServidor.importarServidores(); 
+        console.log('Fim da importacao de dados');
     }
 }
 
