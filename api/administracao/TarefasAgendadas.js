d746ae8e9b3630ee7703bdf422fa172e71ad8f21
gutoconde <gutoconde@gmail.com>
2022-11-20 17:52:35 -0300

    Alterando expressao cron para testes

--- api/administracao/TarefasAgendadas.js
+++ api/administracao/TarefasAgendadas.js
@@ -3,7 +3,7 @@ const cron = require('node-cron');
 const TarefaImportacaoDados = require('../model/TarefaImportacaoDados');
 
 module.exports = (db) => {
-	cron.schedule('00 10 05 * * *', () => {
+	cron.schedule('00 00 18 * * *', () => {
 		const task = new TarefaImportacaoDados();
 		task.executar(db);
 	});
